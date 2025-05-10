012a50aa962626064390b9bcd87db02d21192fd3
tradejuncki65 <tradejunction65@gmail.com>
2025-05-10 23:36:32 +0500

    Create models.py

---
+++ models.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), nullable=False, unique=True)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(256), nullable=False)
+    wallet_address = db.Column(db.String(256))
+    plan = db.Column(db.String(50), default="Free")
+    returns = db.Column(db.Float, default=0.0)
