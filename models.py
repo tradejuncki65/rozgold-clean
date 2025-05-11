cc812a48eb156858d4a11857e18d7293d6fdbc06
tradejuncki65 <tradejunction65@gmail.com>
2025-05-11 13:40:42 +0500

    Update models.py

--- models.py
+++ models.py
@@ -5,9 +5,14 @@ db = SQLAlchemy()
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), nullable=False, unique=True)
-    email = db.Column(db.String(150), nullable=False, unique=True)
+    username = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(256), nullable=False)
+    
+    confirmed = db.Column(db.Boolean, default=False)         # Email verification
+    kyc_status = db.Column(db.String(20), default='Pending') # KYC: Pending, Approved, Rejected
+    
     wallet_address = db.Column(db.String(256))
-    plan = db.Column(db.String(50), default="Free")
+    plan = db.Column(db.String(50))
     returns = db.Column(db.Float, default=0.0)
+
