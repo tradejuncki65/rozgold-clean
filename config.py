b06adfe5f8c8909b99051f84ae727af3a61aeb70
tradejuncki65 <tradejunction65@gmail.com>
2025-05-12 14:40:26 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,17 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'roz_gold_secret_key')
-
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'rtbu updz dugu hsxd ')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # Email Configuration (for Gmail SMTP)
+    # Mail Configuration
     MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')  # set in Render env vars
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')  # set in Render env vars
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = 'tradejunction65@gmail.com'
+    MAIL_PASSWORD = 'rtbu updz dugu hsxd ' 
+    MAIL_DEFAULT_SENDER = 'tradejunction65@gmail.com'
+    MAIL_SUPPRESS_SEND = False
+    MAIL_DEBUG = True
+
